afad04a0436f0bd3c95c110ac2934b19aec72f52
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-05-21 16:15:03 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -102,7 +102,7 @@ echo -e "$green      Install Websocket              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Insshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/xlord27/Autoscript-vpn/main/Insshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 
 #exp
 cd /usr/bin
